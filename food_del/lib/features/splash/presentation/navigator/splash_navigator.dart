a5c462405ddf9b8af0b0564f46e6e4d3c70e724f
danuzz <da.nuzz@danuzzs-MacBook-Air.local>
2024-06-21 14:09:45 +0545

    Remove a  directory

--- food_del/lib/features/splash/presentation/navigator/splash_navigator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:food_del/features/auth/presentation/navigator/login_navigator.dart';
-
-final splashViewNavigatorProvider = Provider <SplashViewNavigator> ((ref)=> SplashViewNavigator());
-
-//this class will be used to navigate to LoginView
-class SplashViewNavigator with LoginViewRoute{}
-
-mixin SplashViewRoute{}
